65d3624655348da56a494b1b95c36bcdccfe5262
Katherine Pannell <katherinepannell@Katherines-MacBook-Pro.local>
2021-05-24 09:50:08 +0200

    fixing Aeropress typo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
       Just some info on different manual brewing methods.
       </p>
       <h2 id="aeropress">Aeropress</h2>
-      <p>Some Aerporess info.</p>
+      <p>Some Aeropress info.</p>
 
       <h3 id="brew-time">Brew time</h3>
       <p>Info on brew time and all that goodness.</p>
