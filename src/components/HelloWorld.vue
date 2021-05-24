bc237c66203da88020d41b083a7e305fe4f72d4e
Katherine Pannell <katherinepannell@Katherines-MacBook-Pro.local>
2021-05-24 10:29:29 +0200

    agaaaaaaiinnn

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
       Just some info on different manual brewing methods.
       </p>
       <h2 id="aeropress">Aeropress</h2>
-      <p>Some more Aeropress info.</p>
+      <p>Some more Aeropress info...again</p>
 
       <h3 id="brew-time">Brew time</h3>
       <p>Info on brew time and all that goodness.</p>
